7c87cca4acb55627447cc806d00292a3f5f84ca4
Ilya Volkov <volkov-ilyha@ya.ru>
2023-11-26 20:33:16 +0300

    feat: добавляет решения задач по теме arrays на странице темы

--- src/components/TaskSolution/components/ConcatenationOfArray.tsx
+++ src/components/TaskSolution/components/ConcatenationOfArray.tsx
@@ -1,7 +1,14 @@
 import type { ReactElement } from 'react';
 
 function ConcatenationOfArray(): ReactElement {
-  return <>Решение</>;
+  return (
+    <p>
+      Создадим переменную <code>ans</code>, которая будет хранить результат вызова
+      функции. Пройдём циклом по входному массиву, на каждой итерации дважды добавляя в{' '}
+      <code>ans</code> каждый элемент: по индексам <code>i</code> и{' '}
+      <code>i + nums.length</code>. Вернём <code>ans</code>.
+    </p>
+  );
 }
 
 export default ConcatenationOfArray;
--- src/components/TaskSolution/components/RemoveDuplicatesFromSortedArray.tsx
+++ src/components/TaskSolution/components/RemoveDuplicatesFromSortedArray.tsx
@@ -1,7 +1,20 @@
 import type { ReactElement } from 'react';
 
 function RemoveDuplicatesFromSortedArray(): ReactElement {
-  return <>Решение</>;
+  return (
+    <>
+      <p>
+        Создадим переменную <code>currentUniqueIndex</code>, в которой будет храниться
+        индекс последнего уникального элемента. Затем в цикле сравним каждый элемент с
+        последним уникальным элементом. Если элементы не равны, инкрементируем значение
+        индекса и кладём текущий элемент по этому индексу.
+      </p>
+      <p>
+        После этого необходимо увеличить значение индекса на 1, чтобы получить количество
+        уникальных элементов.
+      </p>
+    </>
+  );
 }
 
 export default RemoveDuplicatesFromSortedArray;
