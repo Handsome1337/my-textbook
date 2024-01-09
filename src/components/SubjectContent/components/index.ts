0ce2ef66d1759d116cce285bc14dab92bffdfa5d
Ilya Volkov <volkov-ilyha@ya.ru>
2024-01-09 16:33:09 +0300

    feat: добавляет статью про Быструю сортировку

--- src/components/SubjectContent/components/index.ts
+++ src/components/SubjectContent/components/index.ts
@@ -10,6 +10,7 @@ export { default as Objects } from './Objects';
 export { default as OneBranchRecursion } from './OneBranchRecursion';
 export { default as PrefixSums } from './PrefixSums';
 export { default as Queues } from './Queues';
+export { default as QuickSort } from './QuickSort';
 export { default as SlidingWindow } from './SlidingWindow';
 export { default as Stack } from './Stack';
 export { default as TwoBranchRecursion } from './TwoBranchRecursion';
